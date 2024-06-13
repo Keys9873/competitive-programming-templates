f5ffc851bc971cb046b72f7777c9f5ae3d626239
Hori <66225416+Keys9873@users.noreply.github.com>
2024-06-13 14:52:17 -0700

    Update fenwick_tree.h

--- datastructure/fenwick_tree.h
+++ datastructure/fenwick_tree.h
@@ -1,17 +1,9 @@
-struct ft {
-    vector<int> bit; int n;
-    ft(int _n) {bit.resize(_n + 1); n = _n + 1;}
-    void upd(int i, int x) {++i; for (;i < n; i += i & (-i)) bit[i] += x;}
-    int query(int i) {++i; int x = 0; for(; i > 0; i -= i & (-i)) x += bit[i]; return x;}
-    int query(int i, int j) {return query(j) - query(i - 1);}
-};
-
 template <class T>
-struct fenwick_tree {
+struct fenwick {
   int n;
   vector<int> tree;
 
-  fenwick_tree(int _n) : n(_n), tree(n + 5, 0) {}
+  fenwick(int _n) : n(_n + 1), tree(n + 1, 0) {}
 
   void update(int i, T x) {
     for (++i; i < n; i += i & (-i)) {
@@ -30,4 +22,4 @@ struct fenwick_tree {
   T query(int l, int r) {
     return query(r) - (l ? query(l - 1) : 0);
   }
-}
+};
